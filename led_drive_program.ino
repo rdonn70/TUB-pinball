b41b6535d64ca3f6f5daa3dc4ec05203bf576ff5
Ryan Donnellan <69125296+rdonn70@users.noreply.github.com>
2025-02-18 13:34:57 -0500

    Update led_drive_program.ino

--- led_drive_program.ino
+++ led_drive_program.ino
@@ -32,7 +32,7 @@ void loop() {
 
     digitalWrite(5, LOW);
     for(int i = 0; i < 12; i++) {
-      shiftOut(6, 4, LSBFIRST, led_code);
+      shiftOut(6, 4, LSBFIRST, led_code[i]);
     }
     digitalWrite(5, HIGH);
   }
